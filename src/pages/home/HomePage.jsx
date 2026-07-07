e417bddf5c1260ff023427187ed4ffcf9cd87adf
Ayoubkh-maker <ayoubkhetib80@gmail.com>
2026-07-07 15:06:57 +0100

    add backend

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './HomePage.css'
 import { Header } from '../../components/Header';
-import { products } from '../../../starting_code/data/products'
 
 export function HomePage() {
+    const [products ,setProducts ] = useState([]);
+    const [cart ,setCart ] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+               setProducts(response.data);
+            });
+    }, []);
+
+    useEffect(()=> {
+        axios.get('http://localhost:3000/api/cart-items')
+        .then((response)=>{
+             setCart(response.data)
+        })
+    },[])
+
+
+
+
+
     return (
         <>
             <title>Ecommerce Project</title>
-            <Header />
+
+            <Header cart={cart} />
             <div className="home-page">
                 <div className="products-grid">
                     {products.map((product) => {
@@ -25,7 +49,7 @@ export function HomePage() {
                                     <img className="product-rating-stars"
                                         src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                                     <div className="product-rating-count link-primary">
-                                       {product.rating.count}
+                                        {product.rating.count}
                                     </div>
                                 </div>
 
